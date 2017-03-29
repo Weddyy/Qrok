39c20231121812d1e7d70e0fe5182961b2ec8cfd
Paul <mr.weddy@gmail.com>
2017-03-29 16:50:43 +0300

    Commit part 2

--- src/main/java/com/example/configs/SecurityConfig.java
+++ src/main/java/com/example/configs/SecurityConfig.java
@@ -13,6 +13,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll().and().cors().disable();
-
     }
 }
